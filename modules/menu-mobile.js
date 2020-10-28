7f237931f96851e1bed6e6ab878f8cfd58f52235
Muller-Mendonca <mullerm.oficial@gmail.com>
2020-10-28 06:43:20 -0300

    criando menu mobile com javascript

--- modules/menu-mobile.js
+++ modules/menu-mobile.js
@@ -1,4 +1,11 @@
 
 export default function initMenuMobile(){
     console.log('funcionou');
+    const btnHb = document.querySelector('[data-js="btn-hamburguer"]');
+    console.log(btnHb)
+    btnHb.addEventListener('click', openMenu);
+
+    function openMenu(){
+        console.log('Abriu')
+    };
 }
