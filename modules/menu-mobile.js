9bfa25a6066f4888623ef63bb58f5e422f5609da
Muller-Mendonca <mullerm.oficial@gmail.com>
2020-11-16 16:57:17 -0300

    refatorando o touchstart menu-mobile

--- modules/menu-mobile.js
+++ modules/menu-mobile.js
@@ -4,13 +4,11 @@ export default function initMenuMobile(btn, content){
     const btnHb = document.querySelector(btn);
     const nav = document.querySelector(content);
 
-    const events = ['click', 'touchstart'];
-
     function openMenu(){
         nav.classList.toggle('active');
     };
 
     if(window.innerWidth < 600){
-        btnHb.addEventListener(events[1], openMenu);
+        btnHb.addEventListener('touchstart', openMenu);
     };
 };
